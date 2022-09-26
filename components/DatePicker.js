6464195284bcaf4f638c62f32467ec0e96189d17
Finn Formica <finnformica@gmail.com>
2022-09-26 23:20:09 +0100

    uk date format

--- components/DatePicker.js
+++ components/DatePicker.js
@@ -1,20 +1,17 @@
-import { useState } from "react";
-
 import TextField from "@mui/material/TextField";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 export function BasicDatePicker({ label, setValue, value }) {
-  // const [value, setValue] = useState(null);
-
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <DatePicker
         label={label}
         value={value}
+        inputFormat="dd/MM/yyyy"
         onChange={(newValue) => {
-          // setValue(new Date(newValue).toISOString().split("T")[0]);
+          setValue(new Date(newValue).toISOString().split("T")[0]);
         }}
         renderInput={(params) => <TextField {...params} />}
       />
